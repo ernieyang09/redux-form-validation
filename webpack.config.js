a6240309500439a84cb0a1ffc3a44d5fb5fc3958
Ernie Yang <ernieyang09@hotmail.com>
2017-07-25 14:36:16 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  resolve:{
+    modules:['node_modules']
+  },
+  module:{
+    rules: [
+            {
+              loader:'babel-loader',
+              test: /\.jsx?$/,
+              include: path.resolve(__dirname, 'app')
+            }
+    ]
+  },
+  devServer:{
+    port:3000,
+    contentBase:'./src',
+    inline:true
+  },
+  plugins: [
+    HTMLWebpackPluginConfig
+  ]
+}
